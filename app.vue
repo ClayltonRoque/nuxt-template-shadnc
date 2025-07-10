3a69e727254300a3b418fad34d36829126d57fa2
claylton <claylton202@gmail.com>
2025-07-10 18:10:05 -0300

    test

--- app.vue
+++ app.vue
@@ -1,5 +1,52 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+</script>
 
 <template>
-  <fb-Button>hello world</fb-Button>
+  <div class="min-h-screen bg-white px-8 py-8">
+    <div class="mx-auto">
+      <h1 class="text-5xl font-bold text-neutral-grey-08 mb-2">Componentes</h1>
+      <p class="text-xl text-neutral-grey-05 mb-10">
+        Sessão que engloba componentes básicos da UI do projeto
+      </p>
+
+      <section>
+        <h2 class="text-3xl font-bold text-neutral-grey-08 mb-2">Buttons</h2>
+        <div
+          class="text-xs font-bold text-neutral-grey-05 mb-2 uppercase tracking-widest"
+        >
+          Variações
+        </div>
+        <div class="flex flex-row gap-4 mb-8">
+          <fb-Button variant="default">Button</fb-Button>
+          <fb-Button variant="primary">Button</fb-Button>
+          <fb-Button variant="secondary">Button</fb-Button>
+          <fb-Button variant="outline">Button</fb-Button>
+          <fb-Button variant="ghost">Button</fb-Button>
+          <fb-Button variant="disabled">Button</fb-Button>
+        </div>
+
+        <div
+          class="text-xs font-bold text-neutral-grey-05 mb-2 uppercase tracking-widest"
+        >
+          Tamanhos
+        </div>
+        <div class="flex flex-row items-center gap-4">
+          <fb-Button variant="primary" size="lg">Button</fb-Button>
+          <fb-Button variant="primary" size="default">Button</fb-Button>
+          <fb-Button variant="primary" size="sm">Button</fb-Button>
+        </div>
+      </section>
+    </div>
+    <Tabs default-value="account" class="w-[400px]">
+      <TabsList>
+        <TabsTrigger value="account"> Account </TabsTrigger>
+        <TabsTrigger value="password"> Password </TabsTrigger>
+      </TabsList>
+      <TabsContent value="account">
+        Make changes to your account here.
+      </TabsContent>
+      <TabsContent value="password"> Change your password here. </TabsContent>
+    </Tabs>
+  </div>
 </template>
--- components/ui/button/index.ts
+++ components/ui/button/index.ts
@@ -1,36 +1,37 @@
-import { cva, type VariantProps } from 'class-variance-authority'
+import { cva, type VariantProps } from "class-variance-authority";
 
-export { default as Button } from './Button.vue'
+export { default as Button } from "./Button.vue";
 
 export const buttonVariants = cva(
-  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*=\'size-\'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
+  // Ordem: text, flex, layout, border, bg, shadow, hover, disabled
+  "text-sm font-bold inline-flex items-center justify-center gap-2 px-7 py-2 whitespace-nowrap rounded-3xl cursor-pointer transition-all disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
         default:
-          'bg-primary text-primary-foreground shadow-xs hover:bg-primary/90',
-        destructive:
-          'bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
-        outline:
-          'border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50',
+          "text-black border border-neutral-grey-500/50 shadow-xs hover:border-neutral-grey-800",
+        primary:
+          "text-white bg-brand-primary shadow-xs hover:bg-brand-primary-darkest",
         secondary:
-          'bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80',
-        ghost:
-          'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
-        link: 'text-primary underline-offset-4 hover:underline',
+          "text-white bg-brand-secondary shadow-xs hover:bg-brand-secondary-darkest",
+        outline:
+          "text-brand-primary border border-brand-primary-500/50 shadow-xs bg-transparent hover:bg-brand-primary hover:text-white",
+        ghost: "text-brand-primary hover:bg-neutral-grey-200 px-2",
+        link: "text-brand-primary underline-offset-4 hover:underline",
+        disabled:
+          "text-neutral-grey-400 bg-neutral-grey-200 cursor-not-allowed",
       },
       size: {
-        default: 'h-9 px-4 py-2 has-[>svg]:px-3',
-        sm: 'h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5',
-        lg: 'h-10 rounded-md px-6 has-[>svg]:px-4',
-        icon: 'size-9',
+        default: "h-10 py-2",
+        sm: "h-8 py-2 text-xs",
+        lg: "h-12 py-4",
       },
     },
     defaultVariants: {
-      variant: 'default',
-      size: 'default',
+      variant: "default",
+      size: "default",
     },
-  },
-)
+  }
+);
 
-export type ButtonVariants = VariantProps<typeof buttonVariants>
+export type ButtonVariants = VariantProps<typeof buttonVariants>;
